0f3e09574057c5046e3cdc6b751f25b2d508a096
Teng Xiong <txiong11@mail.csuchico.edu>
2020-12-04 23:11:46 -0800

    finish

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -19,7 +19,14 @@ TicTacToeBoard::TicTacToeBoard()
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+  //turn = (turn = X) ? O : X;
+  
+  if(turn==X)
+    turn=O;
+  else
+    turn=X;
+  return turn;
+  
 }
 
 /**
@@ -33,7 +40,15 @@ Piece TicTacToeBoard::toggleTurn()
 **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
-  return Invalid;
+  if(row > 2 || column > 2 || row < 0 || column < 0)
+	  return Invalid;
+  if(board[row][column])
+    return board[row][column];
+  else{
+	  board[row][column]=X;
+    return board[row][column];
+  }
+ // return Invalid;
 }
 
 /**
@@ -42,7 +57,13 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
-  return Invalid;
+  //std::cout<<board[row][column]<<std::endl;
+  if(row>2 || row < 0 || column > 2 || column <0)
+    return Invalid;
+  if(board[row][column])
+    return board[row][column];
+  return Blank;
+  //return Invalid;
 }
 
 /**
@@ -51,5 +72,30 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+  
+  if((board[0][0]==X && board[1][0]==X && board[2][0]==X) || 
+     (board[0][1]==X && board[1][1]==X && board[2][1]==X) || 
+     (board[0][2]==X && board[1][2]==X && board[2][2]==X) ||
+     (board[0][0]==X && board[0][1]==X && board[0][2]==X) ||
+     (board[1][0]==X && board[1][1]==X && board[2][2]==X) ||
+     (board[2][0]==X && board[2][1]==X && board[2][2]==X) ||
+     (board[0][0]==X && board[1][1]==X && board[2][2]==X) ||
+     (board[0][2]==X && board[1][1]==X && board[2][0]==X))
+     return X;
+  else if((board[0][0]==O && board[1][0]==O && board[2][0]==O) || 
+     (board[0][1]==O && board[1][1]==O && board[2][1]==O) || 
+     (board[0][2]==O && board[1][2]==O && board[2][2]==O) ||
+     (board[0][0]==O && board[0][1]==O && board[0][2]==O) ||
+     (board[1][0]==O && board[1][1]==O && board[2][2]==O) ||
+     (board[2][0]==O && board[2][1]==O && board[2][2]==O) ||
+     (board[0][0]==O && board[1][1]==O && board[2][2]==O) ||
+     (board[0][2]==O && board[1][1]==O && board[2][0]==O))
+      return O;
+  else {
+    for(int i=0; i<BOARDSIZE; i++)
+    for(int j=0; j<BOARDSIZE; j++)
+      if(board[i][j] == Blank)
+        return Invalid;
+  }
+  return Blank;
 }
--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "TicTacToeBoard.h"
- 
+#include <iostream> 
 class TicTacToeBoardTest : public ::testing::Test
 {
 	protected:
@@ -14,9 +14,51 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-/* EXAMPLE TEST FORMAT
+// EXAMPLE TEST FORMAT
 TEST(TicTacToeBoardTest, unitTestName)
 {
-	ASSERT_TRUE(true);
+	TicTacToeBoard Board;
+	ASSERT_TRUE(0==Board.toggleTurn());
 }
-*/
+TEST(TicTacToeBoardTest, invalidPlace)
+{
+	TicTacToeBoard testBoard;
+	ASSERT_EQ(testBoard.placePiece(4,1), Invalid);
+}
+TEST(TicTacToeBoardTest, validPlace)
+{
+	TicTacToeBoard testBoard;
+	ASSERT_EQ(testBoard.placePiece(2,1), Invalid);
+}
+TEST(TicTacToeBoard, boardDraw)
+{
+	TicTacToeBoard board; 
+	board.placePiece(0,2);
+	board.placePiece(1,2);
+	board.placePiece(1,0);
+	board.placePiece(2,1);
+	board.placePiece(0,0);
+	board.placePiece(0,1);
+	board.placePiece(1,1);
+	board.placePiece(2,2);
+	board.placePiece(2,0);
+
+	ASSERT_EQ(board.getWinner(), Blank);
+
+}
+TEST(TicTacToeBoard, incomplete_boardDraw)
+{
+	TicTacToeBoard board; 
+	board.placePiece(0,2);
+	board.placePiece(1,2);
+	board.placePiece(1,0);
+	board.placePiece(2,1);
+	board.placePiece(0,0);
+	board.placePiece(0,1);
+	board.placePiece(1,1);
+
+	ASSERT_EQ(board.getWinner(), Blank);
+
+}
+
+
